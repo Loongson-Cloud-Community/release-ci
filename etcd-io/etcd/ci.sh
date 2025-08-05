bf6c98380860f677cfb308695b6fc3a611f45715
yzewei <yangzewei@loongson.cn>
2025-08-05 12:31:05 +0800

    add etcd
    Signed-off-by: yzewei <yangzewei@loongson.cn>

---
+++ etcd-io/etcd/ci.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -eo pipefail
+
+source "$(dirname $0)/lib.sh"
+
+git_commit() 
+{
+    versions=$(echo "$1" | tr '\n' ' ')
+	git add .
+
+    git config user.name "yzewei"
+    git config user.email "yzewei@loongson.cn"
+    git commit -m "Add versions: $versions"
+	git pull --rebase
+    git push origin main
+}
+
+
+main()
+{
+    # 1.获取要构建的版本
+    IFS=$'\n' versions=($(./fetch_versions.sh))
+
+    if [[ -z "$versions" ]]; then
+        log INFO "No versions need updating"
+        return 0
+    else
+        log INFO "Versions needing update: ${versions[@]}"
+    fi
+
+    # 2.执行构建
+    for version in ${versions[@]}
+    do
+        log INFO "Process version $version"
+        ./process_version.sh ${version}
+		update_versions_file "processed_versions.txt" "${version}"
+    done
+
+    # git_commit "${versions[*]}"
+
+    log INFO "All Versions:\n$(cat processed_versions.txt)"
+}
+
+main
+
